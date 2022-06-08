c6f847c8a7b43783dd642034512ab549b824a651
Max Schweikart <hello@maxschweik.art>
2022-06-08 13:52:57 +0200

    content: customize home page

---
+++ components/content/ProfilePicture.vue
@@ -0,0 +1,13 @@
+<template>
+  <img src="/smile.png" style="" class="shadow-lg"/>
+</template>
+
+<style scoped>
+img {
+  display: inline-block;
+  max-width: 128px;
+  border-radius: 2048px;
+  margin: 0;
+  float: right;
+}
+</style>
